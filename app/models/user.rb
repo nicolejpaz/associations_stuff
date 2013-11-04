59ed0a777434e9d6661e5432dad0b58cd9225adf
Nicole Paz <nicole@Nicoles-MacBook-Pro.local>
2013-11-04 10:13:38 -0800

    DRYed up User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,15 @@ class User < ActiveRecord::Base
   has_many :skillsets
   has_many :skills, through: :skillsets
 
+  def make_connection(skill)
+    Skillset.where(user_id: self.id, skill_id: skill.id).first
+  end
+
   def proficiency_for(skill)
-    user_id = self.id
-    skill_id = skill.id
-    connection = Skillset.where(user_id: user_id, skill_id: skill_id)[0]
-    connection[:proficiency]
+    make_connection(skill)[:proficiency]
   end
 
   def set_proficiency_for(skill, rating)
-    user_id = self.id
-    skill_id = skill.id
-    connection = Skillset.where(user_id: user_id, skill_id: skill_id)[0]
-    connection.update_attributes(proficiency: rating)
+    make_connection(skill).update_attributes(proficiency: rating)
   end
 end
